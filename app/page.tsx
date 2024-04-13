9ccbed36ee08628017ab23f20247810056652a79
nanasina <nanasinohomepage@gmail.com>
2024-04-13 12:02:49 +1000

    メインページのデザインを変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import GithubCorner from '@/components/github'
 import CopyBlock, { ocean } from '@/components/codeblock'
-import { Sawarabi_Mincho } from 'next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
 import Logo from '@/app/opengraph-image.png'
 
-const mincho = Sawarabi_Mincho({ weight: '400', subsets: ['latin'] })
+
 
 export default function Home() {
   return (
-    <main className={`${mincho.className} w-full bg-gradient-to-tr from-indigo-200 via-red-200 to-yellow-100 dark:bg-gradient-to-br dark:from-cyan-600 dark:via-cyan-700 dark:to-sky-900 overflow-y-auto p-8`}>
+    <main className="w-full overflow-y-auto p-8 dark:bg-slate-900">
       <Head>
         <meta name='description' content={'Embedding solution for Misskey. Homepage of Missbed.'} />
       </Head>
-      <GithubCorner href='https://github.com/NarixHine/missbed' bannerColor='pink' target='_blank' style={{ opacity: 0.7 }} />
+      <GithubCorner href='https://github.com/NarixHine/missbed' bannerColor='rgb(205, 92, 92)' target='_blank' style={{ opacity: 0.7 }} />
 
       <h1 className='text-5xl text-center'>
         <div className='my-2 relative h-20'>
@@ -24,19 +23,41 @@ export default function Home() {
       <p className='text-center text-slate-500 dark:text-slate-200 italic'>Embedding solution for <a className='text-lime-600'>Misskey</a></p>
       <br></br>
 
-      <div className='w-2/3 bg-slate-200/60 mx-auto rounded-lg opacity-90 p-5 overflow-x-hidden' style={{ minWidth: 280 }}>
+      <div className='w-2/3 bg-slate-200/60 dark:bg-slate-800/100 mx-auto rounded-lg opacity-90 p-5 overflow-x-hidden ' style={{ minWidth: 280 }}>
+        <h1 className="text-2xl font-bold text-gray-800 mb-5 bg-blue-100 dark:text-neutral-200 dark:bg-slate-500 p-3 rounded-lg">ノート</h1>
         <CopyBlock
           // @ts-ignore
-          text={'<iframe src="https://missbed.narix.link/timeline/{instance}/{user_id}" />\n<iframe src="https://missbed.narix.link/timeboard/{instance}/{user_id}" />\n<iframe src="https://missbed.narix.link/note/{instance}/{note_id}" />'}
+          text={'<iframe src="https://missbed.misskey.stream/note/{instance}/{note_id}" />'}
           language={'tsx'}
           showLineNumbers={true}
           theme={{ ...ocean }}
           codeBlock
         />
-        <hr className='my-5 border-purple-400'></hr>
-        <iframe src='/note/firefish.social/9jqn02z9zb8z72rm' width={'100%'} height={700}></iframe>
-        <br></br>
-        <iframe src='/timeline/firefish.social/9jqk8atccfg7sk8x' width={'100%'} height={1000}></iframe>
+        <iframe src='/note/misskey.stream/9p4y4lg5lr' width={'100%'} height={350}></iframe>
+        <br/>
+        <h2 className="text-2xl font-bold text-gray-800 mb-5 bg-blue-50 dark:text-neutral-200 dark:bg-slate-500 p-3 rounded-lg">タイムライン</h2>
+                <CopyBlock
+          // @ts-ignore
+          text={'<iframe src="https://missbed.misskey.stream/timeline/{instance}/{user_id}" />'}
+          language={'tsx'}
+          showLineNumbers={true}
+          theme={{ ...ocean }}
+          codeBlock
+        />
+        <div className='flex justify-center'>
+        <iframe className="" src='/timeline/misskey.stream/9p0tg0nuzo' width={'50%'} height={700}></iframe>
+        </div><br/>
+        <h2 className="text-2xl font-bold text-gray-800 mb-5 bg-blue-100 dark:text-neutral-200 dark:bg-slate-500 p-3 rounded-lg">タイムボード(使用不推奨)</h2>
+        <CopyBlock
+          // @ts-ignore
+          text={'<iframe src="https://missbed.misskey.stream/timeboard/{instance}/{user_id}" />'}
+          language={'tsx'}
+          showLineNumbers={true}
+          theme={{ ...ocean }}
+          codeBlock
+        />
+        <iframe src='/timeboard/misskey.stream/9p0tg0nuzo' width={'100%'} height={1000}></iframe>
+
       </div>
       <br></br>
     </main>
